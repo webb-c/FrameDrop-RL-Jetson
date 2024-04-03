371c98011c6b036c0719e4ad9d21193813455af7
webb-c <jwst0208@gmail.com>
2024-04-03 13:19:14 +0900

    modify: joblib file to pickle

---
+++ utils/joblib_to_pickle.py
@@ -0,0 +1,30 @@
+import os
+import pickle
+import joblib
+
+
+def load_joblib(path):
+    model = joblib.load(path)
+    return model
+
+def save_pickle(path, model):
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    with open(path, 'wb') as file:
+        pickle.dump(model, file)
+    return True
+    
+def joblib_to_pickle(dir_path):
+    file_paths = []
+    for root, directories, files in os.walk(dir_path):
+        for filename in files:
+            file_paths.append(os.path.join(root, filename))
+    
+    for path in file_paths:
+        model = load_joblib(path)
+        save_pickle(path, model)
+
+        
+        
+dir_list = ['model/FrameHopper/cluster/', 'model/LRLO/cluster/']
+for directory in dir_list:
+    joblib_to_pickle(directory)
